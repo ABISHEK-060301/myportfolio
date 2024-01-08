64733631bd614502ad028a1935c984618c073af5
ABISHEKJ-6301 <abishek.j@mitrahsoft.com>
2024-01-08 20:04:52 +0530

    Added Exp & gradients

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,83 @@ import logo from "../src/assets/AJgif.gif";
 import abi from "../src/assets/abi-image-1.jpg";
 import SmoothScroll from "./component/smooth-scroll";
 import gsap from "gsap";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import PlayCircleFilledWhiteTwoToneIcon from "@mui/icons-material/PlayCircleFilledWhiteTwoTone";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
+import { useEffect, useState } from "react";
 gsap.registerPlugin(ScrollToPlugin);
 
 function App() {
+  const joinedMonth = 9;
+  const joinedYear = 2022;
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+  const [className, setClassName] = useState("words");
+
+  useEffect(() => {
+    let temp = className;
+    setIntervals(temp);
+  }, [className]);
+
+  const setIntervals = (temp) => {
+    setTimeout(() => {
+      if (temp === "words") {
+        temp = "";
+        setClassName("");
+      } else {
+        setClassName("words");
+        temp = "words";
+      }
+    }, 4000);
+  };
+
+  const tellMeMonth = (monthNumber) => {
+    switch (monthNumber) {
+      case 0:
+        return "JAN";
+      case 1:
+        return "FEB";
+      case 2:
+        return "MAR";
+      case 3:
+        return "APR";
+      case 4:
+        return "MAY";
+      case 5:
+        return "JUN";
+      case 6:
+        return "JUL";
+      case 7:
+        return "AUG";
+      case 8:
+        return "SEP";
+      case 9:
+        return "OCT";
+      case 10:
+        return "NOV";
+      case 11:
+        return "DEC";
+
+      default:
+        break;
+    }
+  };
   const about = [
     "Hi there,",
     "I'm",
     "Abishek J,",
-    "a highly motivated",
-    "and detail-oriented",
-    "marketing professional",
-    " with over",
+    "a highly motivated,",
+    "detail-oriented",
+    "web and",
+    "mobile app developer",
+    "with over",
     "1.3 years of",
     "experience in",
     "developing and",
@@ -34,8 +99,9 @@ function App() {
     "campaign successful.",
     "Outside of",
     "work, I",
-    "enjoy hiking,",
-    "reading, and",
+    "enjoy exploring",
+    "the world",
+    "with fullest and",
     "spending time",
     "with my family.",
   ];
@@ -66,17 +132,23 @@ function App() {
                       container
                       spacing={2}
                       justifyContent={"space-between"}
-                      alignItems={"center"}
+                      alignItems={""}
                     >
                       <Grid item sm={2}>
                         <img
                           className="pointer"
-                          style={{ padding: "10px 0" }}
+                          style={{ margin: "20px 0" }}
                           src={logo}
                           width={"110vw"}
                         />
                       </Grid>
-                      <Grid item sm={4} spacing={5} padding={0}>
+                      <Grid
+                        item
+                        sm={4}
+                        spacing={5}
+                        padding={0}
+                        style={{ margin: "20px 0" }}
+                      >
                         <Button
                           sx={{
                             fontFamily: "Barlow Semi Condensed",
@@ -118,11 +190,11 @@ function App() {
                           onClick={() => {
                             gsap.to(window, {
                               duration: 1,
-                              scrollTo: { y: "#services", offsetY: 70 },
+                              scrollTo: { y: "#experience", offsetY: 70 },
                             });
                           }}
                         >
-                          SERVICES
+                          EXPERIENCE
                         </Button>
 
                         <Button
@@ -161,23 +233,26 @@ function App() {
                     <div className="content gradient-border-one">
                       <div className="fade-in-content">
                         <Typography
-                          variant="h5"
+                          variant="h4"
                           letterSpacing={3}
                           fontWeight={800}
                           component={"div"}
                           color={"#758398"}
-                          fontFamily={"Lucida Bright, Georgia, serif"}
+                          fontFamily={"League Spartan"}
                         >
-                          {`Hi,I'm`}
+                          {`Hi, I'm`}
                         </Typography>
                         <Typography
+                          fontWeight={800}
                           className="text-effect-one"
                           variant="h2"
                           component={"div"}
                           fontFamily={
-                            "Copperplate,Copperplate Gothic Light,fantasy"
+                            // "Copperplate,Copperplate Gothic Light,fantasy"
+                            "League Spartan"
                           }
-                          letterSpacing={7}
+                          marginTop={2}
+                          letterSpacing={20}
                         >
                           <div>ABISHEK J</div>
                         </Typography>
@@ -187,9 +262,11 @@ function App() {
                         <div className="shadow-body">
                           <Typography
                             variant="h3"
+                            fontWeight={600}
+                            letterSpacing={15}
                             component={"div"}
                             className="text-shine shine-animation-forward shadow-running"
-                            fontFamily={"Nova Flat"}
+                            fontFamily={"Playfair Display"}
                           >
                             <div>FULL STACK</div>
                           </Typography>
@@ -197,9 +274,11 @@ function App() {
                         <div className="shadow-body">
                           <Typography
                             variant="h3"
+                            fontWeight={800}
+                            letterSpacing={2}
                             component={"div"}
                             className="text-shine shine-animation-backward shadow-running"
-                            fontFamily={"Nova Flat"}
+                            fontFamily={"Playfair Display"}
                           >
                             <div>WEB DEVELOPER</div>
                           </Typography>
@@ -220,7 +299,7 @@ function App() {
                             height={"500dvh"}
                           />
                         </div>
-                        <div className="roundness"></div>
+                        <div className="roundness roundness-image"></div>
                       </Box>
                     </Grid>
                   </Grid>
@@ -241,7 +320,7 @@ function App() {
           {/*--------------------------------- End of Home------------------------------ */}
 
           {/* About */}
-          <div id="">
+          <div id="about">
             <Box marginTop={20}>
               <Container>
                 <Grid
@@ -280,7 +359,7 @@ function App() {
                             </Typography>
                           </div>
                         </div>
-                        <div className="roundness"></div>
+                        <div className="roundness roundness-about"></div>
                       </Box>
                     </Grid>
                   </Grid>
@@ -297,7 +376,7 @@ function App() {
                       {about.map((word, index) => (
                         <span
                           key={`${index * 12}-word`}
-                          className="words"
+                          className={className}
                         >{` ${word} `}</span>
                       ))}
                     </Typography>
@@ -316,25 +395,150 @@ function App() {
             </Box>
           </div>
 
-          <div id="services">
-            {/* <Box>
+          <div id="experience">
+            <Box marginTop={20}>
+              <Container>
+                <Grid
+                  container
+                  justifyContent={"space-between"}
+                  alignItems={"flex-start"}
+                >
+                  <Grid item sm={6} maxHeight={550}>
+                    <Grid container justifyContent={"end"}>
+                      <Timeline position="right">
+                        <TimelineItem>
+                          <TimelineOppositeContent
+                            sx={{ m: "20px auto" }}
+                            align="right"
+                            variant="body1"
+                            color="white"
+                          >
+                            {`${joinedYear} - ${tellMeMonth(joinedMonth)}`}
+                          </TimelineOppositeContent>
+
+                          <TimelineSeparator>
+                            {/* <TimelineConnector /> */}
+                            <TimelineDot
+                              sx={{
+                                bgcolor: "#749842",
+                              }}
+                              variant="filled"
+                            >
+                              <PlayCircleFilledWhiteTwoToneIcon fontSize="large" />
+                            </TimelineDot>
+                            <TimelineConnector sx={{ bgcolor: "#749842" }} />
+                          </TimelineSeparator>
+
+                          <TimelineContent sx={{ py: "12px", px: 2 }}>
+                            <Typography
+                              variant="h6"
+                              component="span"
+                              color="white"
+                            >
+                              MitrahSoft pvt ltd.
+                            </Typography>
+                            <Typography color="grey">
+                              Company which gave me the first opportunity to
+                              enrich myself in all stacks!
+                            </Typography>
+                          </TimelineContent>
+                        </TimelineItem>
+
+                        <TimelineItem>
+                          <TimelineOppositeContent
+                            align="center"
+                            sx={{ m: "20px auto" }}
+                            variant="body1"
+                            color="white"
+                          >
+                            {`${currentYear} - ${tellMeMonth(currentMonth)}`}
+                          </TimelineOppositeContent>
+
+                          <TimelineSeparator>
+                            <TimelineConnector sx={{ bgcolor: "#749842" }} />
+                            <TimelineDot
+                              sx={{
+                                bgcolor: "#749842",
+                              }}
+                              variant="filled"
+                            >
+                              <LaptopMacIcon fontSize="large" />
+                            </TimelineDot>
+                          </TimelineSeparator>
+
+                          <TimelineContent sx={{ py: "12px", px: 2 }}>
+                            <Typography
+                              variant="h6"
+                              component="span"
+                              color="white"
+                            >
+                              Still Coding here!
+                            </Typography>
+                            <Typography color="grey">
+                              Because it&apos;s awesome!
+                            </Typography>
+                          </TimelineContent>
+                        </TimelineItem>
+                      </Timeline>
+                    </Grid>
+                  </Grid>
+
+                  <Grid item sm={6} maxHeight={550}>
+                    <Box>
+                      <div className="float gradient-border">
+                        <div className="word-content">
+                          <Typography
+                            id="about"
+                            variant="h3"
+                            className="word"
+                            letterSpacing={3}
+                            fontWeight={800}
+                            component={"div"}
+                            color={"#000000"}
+                            fontFamily={"Poppins, sans-serif"}
+                          >
+                            EXPERIENCE
+                          </Typography>
+                          <Typography
+                            id="about"
+                            variant="h3"
+                            className="word"
+                            letterSpacing={3}
+                            fontWeight={800}
+                            component={"div"}
+                            color={"#000000"}
+                            fontFamily={"Poppins, sans-serif"}
+                          >
+                            EXPERIENCE
+                          </Typography>
+                        </div>
+                      </div>
+                      <div className="roundness roundness-exp"></div>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Container>
+            </Box>
+
+            {/* HR */}
+            <Box>
               <Container>
                 <Grid container justifyContent={"center"}>
                   <hr className="hr-small" />
                 </Grid>
               </Container>
-            </Box> */}
+            </Box>
           </div>
 
-          <div id="contact">
-            {/* <Box>
+          {/* <div id="contact"> */}
+          {/* <Box>
               <Container>
                 <Grid container justifyContent={"center"}>
                   <hr className="hr-small" />
                 </Grid>
               </Container>
             </Box> */}
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </SmoothScroll>
